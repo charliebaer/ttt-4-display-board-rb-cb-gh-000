dc8ecab6b95efa601f068af59e38b65e29ad2764
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-05 18:59:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(b)
+def display_board(board)
 puts "b[1]|   |  "
 puts "-----------"
 puts "   |   |   "
